da916ea9c6b9ec9ffbcc481320c17df253ed2f37
Sheenaaaa <156189607+Sheenaaaa@users.noreply.github.com>
2024-03-02 10:10:00 +0800

    Add files via upload

---
+++ server/schemas/index.ts
@@ -0,0 +1,6 @@
+import beverages from './beverages';
+import breakfast from './breakfast'
+import dessert from './dessert';
+import lunch from "./lunch";
+import dinner from "./dinner"
+export const schemaTypes = [beverages, breakfast, dessert, lunch, dinner]
